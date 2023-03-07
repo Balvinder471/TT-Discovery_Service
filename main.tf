c1757629b5fafd7f06e60598774968549e504322
Balvinder471 <sbalvinder471@gmail.com>
2023-03-07 14:44:16 +0530

    Bug fix

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http.id]
   tags = {
     Name = "HelloWorld"
   }
