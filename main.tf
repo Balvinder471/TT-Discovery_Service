5f27bf36ea829eb246898f3fd9a2a50a2eaa79bf
Balvinder471 <sbalvinder471@gmail.com>
2023-03-07 14:39:26 +0530

    Added Security Group

--- main.tf
+++ main.tf
@@ -14,11 +14,38 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t2.micro"
-
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "http" {
+  name = "allow-http"
+  description = "Allow http in and all out"
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "http_in" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.http.id
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "all_out" {
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.http.id
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks = ["0.0.0.0/0"]
 }
